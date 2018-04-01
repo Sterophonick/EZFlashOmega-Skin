fd0505afcbf03d269a9a650d38d47c6dc36a643f
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2018-04-01 16:39:47 -0700

    Add files via upload

--- omegaskin.asm
+++ omegaskin.asm
@@ -1,6 +1,12 @@
 .gba
 .open "ezkernel.bin",0x8000000
 
-.org 0x80D1069 :: .incbin "strings1.bin"
+.org 0x80D1069 :: .incbin "strings1.bin" //Changes strings
+.org 0x80BAC48 :: .incbin "title.bin" //Changes title screen
+.org 0x80221cd :: .incbin "font.bin" //Changes font
+.org 0x80FBEC9 :: .incbin "strings2.bin" //Updates more strings
+.org 0x80FBDFC :: .byte 0x53 //Changes the name of the folder containing the Save data
+.org 0x804F008 :: .incbin "pocketnes.bin" //Updates PocketNES
+.org 0x80000A0 :: .incbin "header.bin" //Changes ROM Header
 
 .close
